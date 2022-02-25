1c80237d322f6029786c83e04752fd4965f6a055
eddypurwono <99484195+eddypurwono@users.noreply.github.com>
2022-02-25 21:42:24 +0700

    Create calculate.js

---
+++ calculate.js
@@ -0,0 +1,10 @@
+var tombol = document.querySelector(".box");
+ var input = document.querySelector(".input") ;
+	tombol.addEventListener('click', function(data){
+       
+		  var nilaiklik=data.target.innerText;
+		     if(nilaiklik == "C") { input.value ="0"; }
+		     else if(nilaiklik == "D") { input.value=input.value.slice('0', -1);}
+               else if(nilaiklik == "=") { input.value = eval(input.value);}
+            else{ input.value= input.value+ nilaiklik;}
+});
